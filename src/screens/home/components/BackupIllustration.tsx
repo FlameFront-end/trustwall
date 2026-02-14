538148365dec6afa5798214d3f81df93703ff20a
FlameFront-end <flamekaliganov@example.com>
2026-02-14 23:24:40 +0800

    fix: inline backup illustration into single-file build

--- src/screens/home/components/BackupIllustration.tsx
+++ src/screens/home/components/BackupIllustration.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react'
 
+import backupImg from '@/shared/assets/backup-illustration.png'
 import s from './BackupIllustration.module.scss'
 
 export const BackupIllustration: FC = () => (
   <div className={s.wrapper}>
     <img
-      src="backup-illustration.png"
+      src={backupImg}
       alt="Back up secret phrase"
       className={s.illustration}
     />
